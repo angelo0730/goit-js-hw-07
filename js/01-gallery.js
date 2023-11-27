89ac0b86084c8ef11050d911f6cb536982771096
angelo0730 <137179844+angelo0730@users.noreply.github.com>
2023-11-27 23:45:56 +0800

    update changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGallery = (element) => {
+    return element.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+};
+
+const photosList = createGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", photosList);
+
+// ----------Click Event-----------
+
+const eventGalleryClick = (event) => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+
+    }
+
+    const urlOriginal = event.target.dataset.source;
+
+
+const instance = basicLightbox.create(`<img src="${urlOriginal}">`);
+instance.show();
+
+// -----ESC instance-----
+const escKeyPress = (event) => {
+    if (event.key === "Escape") {
+        instance.close();
+        window.removeEventListener("keydown", escKeyPress)
+    }
+};
+window.addEventListener("keydown", escKeyPress)
+
+
+};
+galleryList.addEventListener("click", eventGalleryClick);
